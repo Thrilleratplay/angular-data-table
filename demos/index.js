3a4c138c2516a19f8fe5774ff2168db48d8612b3
Tom Hiller <thrilleratplay@gmail.com>
2017-08-30 20:58:42 -0400

    revert demos to swimlane

--- demos/index.js
+++
@@ -1,9 +0,0 @@
-import 'angular';
-
-import '../src/dataTable';
-
-import '../src/dataTable.css';
-import '../src/themes/icons.css';
-import '../src/components/menu/menu.css';
-import '../src/components/popover/popover.css';
-import '../src/themes/material.css';
